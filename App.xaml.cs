53abfa25bc51643cbcb13a98e94b8cb963ef31cf
opilane <opilane@HP-CZC2349HTZ>
2025-04-09 13:05:52 +0300

    Muuta

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,27 +2,28 @@
 
 namespace Tervisepaevik_Valeria_Daria
 {
-    //public const string DATABASE_NAME = "Tervisepaevik.db";
-    //public static KasutajadRepository database;
-    //public static KasutajadRepository Database
-    //{
-    //    get
-    //    {
-    //        if (database == null)
-    //        {
-    //            database = new KasutajadRepository(
-    //                Path.Combine(
-    //                    Environment.GetFolderPath(Enviro0)))
-    //        }
-    //    }
-    //}
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "Tervisepaevik.db";
+        public static KasutajadRepository database;
+        public static KasutajadRepository Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new KasutajadRepository(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new DBListPage());
         }
     }
 }
