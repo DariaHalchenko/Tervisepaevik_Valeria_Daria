1e21acd7903459b06581b2b4d0914facbd63ecff
opilane <opilane@HP-CZC2349HTZ>
2025-04-11 12:32:12 +0300

    Muuta

--- Views/RegistrationPage.xaml.cs
+++ Views/RegistrationPage.xaml.cs
@@ -6,11 +6,12 @@ public partial class RegistrationPage : ContentPage
 {
 	Label lbl_nimi, lbl_vanus, lbl_email, lbl_parool, lbl_kaal, lbl_pikkus;
 	Entry e_nimi, e_vanus, e_email, e_parool, e_kaal, e_pikkus;
-	Button btn_salvesta, btn_kustuta, btn_exit;
+	Button btn_salvesta, btn_kustuta, btn_exit, btn_List;
     StackLayout sl;
 	public RegistrationPage()
 	{
-		lbl_nimi = new Label { Text = "Nimi: " };
+        this.BindingContext = new Kasutajad();
+        lbl_nimi = new Label { Text = "Nimi: " };
         lbl_vanus = new Label { Text = "Vanus: " };
         lbl_email = new Label { Text = "Email: " };
         lbl_parool = new Label { Text = "Parool: " };
@@ -35,21 +36,30 @@ public partial class RegistrationPage : ContentPage
         btn_salvesta.Clicked += Btn_salvesta_Clicked;
         btn_kustuta = new Button
         {
-            Text = "Salvesta"
+            Text = "Kustuta"
         };
         btn_kustuta.Clicked += Btn_kustuta_Clicked;
         btn_exit = new Button
         {
-            Text = "Salvesta"
+            Text = "Exit"
         };
         btn_exit.Clicked += Btn_exit_Clicked;
-
+        btn_List = new Button
+        {
+            Text = "List"
+        };
+        btn_List.Clicked += Btn_List_Clicked; ;
         sl = new StackLayout
         {
             Children = { lbl_nimi, e_nimi, lbl_vanus, e_vanus, lbl_email, e_email, lbl_parool, e_parool, lbl_kaal, e_kaal, lbl_pikkus, e_pikkus,
-            btn_salvesta, btn_kustuta, btn_exit}
+            btn_salvesta, btn_kustuta, btn_exit, btn_List}
         };
         Content = sl;
+    }
+
+    private void Btn_List_Clicked(object? sender, EventArgs e)
+    {
+        this.Navigation.PushAsync(new DBListPage());
     }
 
     private void Btn_exit_Clicked(object? sender, EventArgs e)
@@ -67,7 +77,7 @@ public partial class RegistrationPage : ContentPage
     private void Btn_salvesta_Clicked(object? sender, EventArgs e)
     {
         var kasutajad = (Kasutajad)BindingContext;
-        if (!String.IsNullOrEmpty(kasutajad.Nimi))
+        if (kasutajad!= null && !String.IsNullOrEmpty(kasutajad.Nimi))
         {
             App.Database.SaveItem(kasutajad);
         }
